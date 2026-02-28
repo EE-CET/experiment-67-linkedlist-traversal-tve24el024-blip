c979e1cfa324838e5600d5a00d84a6fbbadae455
tve24el024-blip <tve24el024@cet.ac.in>
2026-02-28 22:38:11 +0530

    added

--- src/Solution.java
+++ src/Solution.java
@@ -2,22 +2,38 @@ import java.util.LinkedList;
 import java.util.ListIterator;
 import java.util.Scanner;
 
-public class Solution {
+public class LinkedListTraversal {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        
-        // TODO: Read the integer N
-        
-        // TODO: Create a LinkedList of Integers
-        
-        // TODO: Read N integers and add them to the LinkedList
-        
-        // TODO: Create a ListIterator for the LinkedList
-        
-        // TODO: Traverse the list in the forward direction and print the elements
-        
-        // TODO: Traverse the list in the backward direction and print the elements
-        
-        
+
+        // Step 1: Read input
+        Scanner input = new Scanner(System.in);
+
+        int n = input.nextInt();   // Number of elements
+
+        // Step 2: Create LinkedList
+        LinkedList<Integer> list = new LinkedList<>();
+
+        // Step 3: Add elements to list
+        for (int i = 0; i < n; i++) {
+            list.add(input.nextInt());
+        }
+
+        // Step 4: Create ListIterator
+        ListIterator<Integer> iterator = list.listIterator();
+
+        // Step 5: Forward traversal
+        System.out.print("Forward: ");
+        while (iterator.hasNext()) {
+            System.out.print(iterator.next() + " ");
+        }
+        System.out.println();
+
+        // Step 6: Backward traversal
+        System.out.print("Backward: ");
+        while (iterator.hasPrevious()) {
+            System.out.print(iterator.previous() + " ");
+        }
+
+        input.close();
     }
 }
