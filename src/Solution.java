50749b8a07e403b71907df10ab28d45b605f4651
tve24el024-blip <tve24el024@cet.ac.in>
2026-02-28 22:41:01 +0530

    added

--- src/Solution.java
+++ src/Solution.java
@@ -6,28 +6,30 @@ public class Solution {
     public static void main(String[] args) {
 
         Scanner input = new Scanner(System.in);
-
         int n = input.nextInt();
 
         LinkedList<Integer> list = new LinkedList<>();
-
         for (int i = 0; i < n; i++) {
             list.add(input.nextInt());
         }
 
-        ListIterator<Integer> iterator = list.listIterator();
+        ListIterator<Integer> it = list.listIterator();
 
-        // Forward traversal
+        // Forward
         System.out.print("Forward: ");
-        while (iterator.hasNext()) {
-            System.out.print(iterator.next() + " ");
+        while (it.hasNext()) {
+            int val = it.next();
+            System.out.print(val);
+            if (it.hasNext()) System.out.print(" ");
         }
         System.out.println();
 
-        // Backward traversal
+        // Backward
         System.out.print("Backward: ");
-        while (iterator.hasPrevious()) {
-            System.out.print(iterator.previous() + " ");
+        while (it.hasPrevious()) {
+            int val = it.previous();
+            System.out.print(val);
+            if (it.hasPrevious()) System.out.print(" ");
         }
 
         input.close();
